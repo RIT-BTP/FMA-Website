c124b3118a82a534649950a537c01178ce641f6c
Simon Young <simon@simons-mbp.wireless.rit.edu>
2019-09-24 15:06:54 -0400

    Black Formated

--- app/app.py
+++ app/app.py
@@ -15,6 +15,7 @@ db = SQLAlchemy(app)
 
 from models import Stocks
 from forms import StockEntryForm
+
 # from functions import user_check
 
 
@@ -22,17 +23,22 @@ from forms import StockEntryForm
 def root():
     return redirect(url_for("home"))
 
+
 @app.route("/home")
 def home():
     return render_template("home.html")
 
 
-@app.route("/new stocks", methods=['GET', 'POST'])
+@app.route("/new stocks", methods=["GET", "POST"])
 def new_stocks():
     form = StockEntryForm(request.form)
-    if request.method == 'POST' and form.validate():
-        Stocks.insert(name=form.ticker.data, quantity=form.quantity.data, cost=form.cost.data, index=form.index.data, sector=form.sector.data)
+    if request.method == "POST" and form.validate():
+        Stocks.insert(
+            name=form.ticker.data,
+            quantity=form.quantity.data,
+            cost=form.cost.data,
+            index=form.index.data,
+            sector=form.sector.data,
+        )
         return redirect(url_for("home"))
     return render_template("new_stocks.html", form=form)
-
-
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,21 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators, FileField, TextAreaField, IntegerField, DecimalField
+from wtforms import (
+    Form,
+    BooleanField,
+    StringField,
+    PasswordField,
+    validators,
+    FileField,
+    TextAreaField,
+    IntegerField,
+    DecimalField,
+)
 from wtforms.fields.html5 import DateField
 
+
 class StockEntryForm(Form):
-    ticker = StringField("Ticker", [validators.Length(min=1,max=10), validators.DataRequired()])
+    ticker = StringField(
+        "Ticker", [validators.Length(min=1, max=10), validators.DataRequired()]
+    )
     quantity = IntegerField("Quantity", [validators.DataRequired()])
     cost = DecimalField("Cost", [validators.DataRequired()])
     index = StringField("Financial Index", [validators.length(min=2, max=20)])
