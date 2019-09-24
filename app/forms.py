6ee2432f03f393bdb15e135831e827efd2ef64f4
Simon Young <simon@simons-mbp.wireless.rit.edu>
2019-09-24 15:04:19 -0400

    Initial Website commit

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, FileField, TextAreaField, IntegerField, DecimalField
+from wtforms.fields.html5 import DateField
+
+class StockEntryForm(Form):
+    ticker = StringField("Ticker", [validators.Length(min=1,max=10), validators.DataRequired()])
+    quantity = IntegerField("Quantity", [validators.DataRequired()])
+    cost = DecimalField("Cost", [validators.DataRequired()])
+    index = StringField("Financial Index", [validators.length(min=2, max=20)])
+    sector = StringField("Financial Sector", [validators.length(min=1, max=20)])
