d078c99d1c14ecb97177d689a4010ec73ab995db
Simon Young <simon@Simons-MacBook-Pro.local>
2019-11-19 11:45:15 -0500

    auto update ticker bar

---
+++ app/static/application.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    //connect to the socket server.
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/stock-api');
+    var numbers_received = [];
+
+    //receive details from server
+    socket.on('newprice', function(msg) {
+        console.log("Reveiced Payload" + msg);
+        $('#dow-chng').html(msg.DOW)
+        if (msg.DOW.substring(0,1) == "-"){
+            $('#dow-chng').css({'color':'red'});
+        } else {
+            $('#dow-chng').css({'color':'green'});
+        }
+        
+        $('#nas-chng').html(msg.NASDAQ)
+        if (msg.NASDAQ.substring(0,1) == "-"){
+            $('#nas-chng').css({'color':'red'});
+        } else {
+            $('#nas-chng').css({'color':'green'});
+        }
+
+        $('#sp-chng').html(msg.SP500)
+        if (msg.SP500.substring(0,1) == "-"){
+            $('#sp-chng').css({'color':'red'});
+        } else {
+            $('#sp-chng').css({'color':'green'});
+        }
+    });
+
+});
